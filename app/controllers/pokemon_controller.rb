fef42c8bd2c6554fc5d87a82d3ff8e20ea63e91a
DianaBianca <dianabiancasr@gmail.com>
2022-10-22 19:29:30 -0300

    first commit

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,4 @@
+class PokemonController < ApplicationController
+    def new_aquisition
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  get '/aquisition_pokemon', to: 'pokemon#new_aquisition'
+
+end
