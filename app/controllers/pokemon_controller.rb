df0c9fbe3247d6d9b3847e710bdd819235fca5aa
DianaBianca <dianabiancasr@gmail.com>
2022-10-23 14:53:41 -0300

    WIP new aquisition view

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,4 +1,45 @@
 class PokemonController < ApplicationController
-    def new_aquisition
+  def new_aquisition
+  
+    @pokemons_data = fetch_pokemons_info#[{name: , type: , price: experience_quotation(experience, btc_price)}]
+  end
+
+  private
+
+  def fetch_pokemons_info
+    #pega as infos: name, type, experience_quotation da API e retorna em um array de hash fprmatado
+    response = Faraday.get("https://pokeapi.co/api/v2/pokemon")
+    data = JSON.parse(response.body)["results"]
+    poke_names = data.map{|pokemon| pokemon["name"]}
+
+    pokemon_data(poke_names)
+  end
+
+  def  pokemon_data(poke_names)
+    values = poke_names.map do |name|
+      
+      data = fetch_pokemon_data(name)
+
+      photo = data["sprites"]["other"]["dream_world"]["front_default"]
+      type = data["types"].map{|info| info["type"]["name"]}
+
+      { name: name, photo: photo,type: type, price: experience_quotation(data["base_experience"]).ceil(2) }
     end
+  end
+
+  def btc_quotation
+    response = Faraday.get("https://www.mercadobitcoin.net/api/BTC/ticker/")
+    JSON.parse(response.body)["ticker"]["last"].to_f
+  end  
+
+  def experience_quotation(experience)
+    btc_quotas = experience * 0.00000001
+    final_price = btc_quotas * btc_quotation
+  end
+
+  def fetch_pokemon_data(name)
+    response = Faraday.get("https://pokeapi.co/api/v2/pokemon/#{name}")
+    JSON.parse(response.body)
+  end
+
 end
